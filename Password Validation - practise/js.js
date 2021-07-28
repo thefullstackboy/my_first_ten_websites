3898d31c5727d4353ba648aa0293a2b8cea22068
thefullstackboy <deepaksewani03@gmail.com>
2021-07-28 15:36:41 +0530

    vf

---
+++ Password Validation - practise/js.js
@@ -0,0 +1,15 @@
+var passwordbox=document.getElementById("psw");
+var letter=document.getElementById("letter");
+var capital=document.getElementById("capital");
+var number=document.getElementById("number");
+var length=document.getElementById("length");
+
+function passwordboxcli(){
+    passwordbox.style.display= "block";
+}
+
+function boxout(){
+    passwordbox.style.display= "none";
+}
+
+
