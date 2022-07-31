15dbdcfee5b2d811f86202001335c1d58f3fd12f
thefullstackboy <66158784+thefullstackboy@users.noreply.github.com>
2022-07-31 14:05:48 +0530

    use ref

--- react course/React/contextapp/src/App.js
+++ react course/React/contextapp/src/App.js
@@ -1,15 +1,56 @@
 import React from 'react'
 import './App.css';
-import ParentComponent from './Components/ParentComponent'
-
+//import ParentComponent from './Components/ParentComponent'
+import FocusInput from './Components/FocusInput';
 export default function App() {
   return (
     <div className="App">
-      <ParentComponent/>
+      {/* <ParentComponent/> */}
+      <FocusInput/>
     </div>
   )
 }
 
+// import React, {useState, useMemo} from 'react';
+
+// function App() {
+// const [number, setNumber] = useState(0)
+// // Using useMemo
+// const squaredNum = useMemo(()=> {
+// 	return squareNum(number);
+// }, [number])
+// const [counter, setCounter] = useState(0);
+
+// // Change the state to the input
+// const onChangeHandler = (e) => {
+// 	setNumber(e.target.value);
+// }
+	
+// // Increases the counter by 1
+// const counterHander = () => {
+// 	setCounter(counter + 1);
+// }
+// return (
+// 	<div className="App">
+// 	<h1>Welcome to Geeksforgeeks</h1>
+// 	<input type="number" placeholder="Enter a number"
+// 		value={number} onChange={onChangeHandler}>
+// 	</input>
+		
+// 	<div>OUTPUT: {squaredNum}</div>
+// 	<button onClick= {counterHander}>Counter ++</button>
+// 	<div>Counter : {counter}</div>
+// 	</div>
+// );
+// }
+
+// // function to square the value
+// function squareNum(number){
+// console.log("Squaring will be done!");
+// return Math.pow(number, 2);
+// }
+
+// export default App;
 
 
 
