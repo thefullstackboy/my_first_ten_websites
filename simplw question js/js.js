7123c3fdcd9974f65a1dc1f6e00a014038a960b8
thefullstackboy <66158784+thefullstackboy@users.noreply.github.com>
2023-10-01 21:01:20 +0530

    project

---
+++ simplw question js/js.js
@@ -0,0 +1,55 @@
+// quesht 3 nahi aaya 
+console.log("start")
+
+// sum of two numbers**********************************
+// const sumFunc = (a,b)=> {
+//  return a + b;
+// }
+// console.log(sumFunc(132,880))
+//*******************************************************/
+
+//find maximum number 
+// findMinMax()************************
+// function findMinMax(Arr) {
+//     return Arr = Math.max(...Arr); 
+// }
+//  console.log(findMinMax([25,40,4500000,48,98,98,105,85,809,915,981,105,100000,100005]))
+//*******************************************************/
+
+
+//reverse string**********************************
+// function reverseStrint(string){
+//     return string.split('').reverse().join('')
+// }
+// console.log(reverseStrint("my name is deepak"))
+//*******************************************************/
+
+//. Write a JavaScript function that takes an array of numbers and returns a new array with only the even numbers. 
+// function returnEvenNumber(numberArray){
+//     return numberArray.filter(num => num % 2 === 0); 
+// }
+
+// console.log(returnEvenNumber([5,2,4,6,8,0,18,5,14,16,24,32,34,100]))
+
+//*********************************************************** */
+// Write a JavaScript program to calculate the factorial of a given number. 
+// function factorial(number) { 
+//     if (number === 0 || number === 1) {   
+//       return 1;   
+//     } else {   
+//       return number * factorial(number - 1);   
+//     }   
+//   } 
+//   console.log(factorial(5))
+//************************************************************/
+
+
+// 7. Write a JavaScript function to check if a given number is prime. 
+// function isPrime(num) { 
+//     if (num <= 1) return false;   
+//     for (let i = 2; i <= Math.sqrt(num); i++) {  
+//       if (num % i === 0) return false;   
+//     }   
+//     return true;   
+//   } 
+// console.log(isPrime(92))
