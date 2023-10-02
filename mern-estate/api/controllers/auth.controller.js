d707f04f5ccdddc7d1ed1572f05856550faae01f
thefullstackboy <66158784+thefullstackboy@users.noreply.github.com>
2023-10-02 20:50:16 +0530

    all

--- mern-estate/api/controllers/auth.controller.js
+++ mern-estate/api/controllers/auth.controller.js
@@ -5,7 +5,7 @@ import  jwt  from 'jsonwebtoken';
 
 export const signup = async(req, res, next) =>{
   const { username, email, password } = req.body;
-  const hashedPassword = bcrypt.hashSync(password, 10);
+  const hashedPassword = bcryptjs.hashSync(password, 10);
   const newUser = new User({ username, email, password:hashedPassword});
   try {
     await newUser.save();
@@ -20,7 +20,7 @@ export const signin = async(req,res, next) => {
   try {
       const validUser = await User.findOne({ email });
       if(!validUser) return next(errorHandler(404, 'User not found!'));
-      const validPassword = bcrypt.compareSync(password, validUser.password);
+      const validPassword = bcryptjs.compareSync(password, validUser.password);
       if(!validPassword) return next(errorHandler(401, 'Wrong credentials'));
       const token = jwt.sign({ id: validUser._id}, process.env.JWT_SECRET);
       const {password: pass, ...rest} = validUser._doc;
