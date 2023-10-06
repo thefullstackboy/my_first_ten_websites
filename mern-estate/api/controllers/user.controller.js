5ff450a0b51a2057d7e8d093b05b6ca4131eb77c
thefullstackboy <66158784+thefullstackboy@users.noreply.github.com>
2023-10-06 20:19:38 +0530

    mern project

--- mern-estate/api/controllers/user.controller.js
+++ mern-estate/api/controllers/user.controller.js
@@ -1,5 +1,6 @@
 import bcryptjs from 'bcryptjs';
 import User from '../models/user.model.js';
+import Listing from '../models/listing.model.js';
 import { errorHandler } from '../utils/error.js';
 
 
@@ -50,3 +51,25 @@ export const deleteUser = async (req, res, next) => {
     next(error)
   }
 }
+
+export const createListing = async (req, res, next) => {
+  try {
+    const listing = await Listing.create(req.body);
+    return res.status(201).json(listing);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getUserListings = async (req, res, next) => {
+  if (req.user.id === req.params.id) {
+    try {
+      const listings = await Listing.find({ userRef: req.params.id });
+      res.status(200).json(listings);
+    } catch (error) {
+      next(error);
+    }
+  } else {
+    return next(errorHandler(401, 'You can only view your own listings!'));
+  }
+};
